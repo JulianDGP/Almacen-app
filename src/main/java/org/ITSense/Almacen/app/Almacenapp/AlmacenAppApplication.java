1c358d60afabcd3464f89f96ed45a4ab5e1642a9
JulianDGP <juliandgp@hotmail.com>
2023-10-02 16:34:17 -0500

    Se crea el proyecto con las 4 dependencias principales

---
+++ src/main/java/org/ITSense/Almacen/app/Almacenapp/AlmacenAppApplication.java
@@ -0,0 +1,13 @@
+package org.ITSense.Almacen.app.Almacenapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ITSense/Almacen/app/Almacenapp/AlmacenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ITSense.Almacen.app.Almacenapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
